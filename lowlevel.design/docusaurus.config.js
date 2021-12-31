febf1b43de1b84c80de473e63d8b0cb3cf65ea9c
Naman Bhalla <namanbhalla1998@gmail.com>
2022-01-01 02:57:46 +0530

    Initial Examples and Tutorial Setup

--- lowlevel.design/docusaurus.config.js
+++ lowlevel.design/docusaurus.config.js
@@ -125,7 +125,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Naman Bhalla`,
       },
       prism: {
         theme: lightCodeTheme,
